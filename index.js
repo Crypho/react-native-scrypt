a7b26346392d2ce72ad81a645d31227c74a3d013
Yiorgis Gozadinos <ggozad@crypho.com>
2017-11-24 08:44:39 +0100

    Skeleton for iOS

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import { NativeModules } from 'react-native'
 
 const { RNScrypt } = NativeModules
 
-async function scrypt (passwd, salt, N, r, p, dkLen) {
-  alert(RNScrypt)
+async function scrypt (passwd, salt, N=16384, r=8, p=1, dkLen=64) {
   const retval:string = await RNScrypt.scrypt(passwd, salt, N, r, p, dkLen)
   return retval
 }
