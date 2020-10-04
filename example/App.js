451602a7e041ec309c4f800dcddf3148143fdce6
tex0l <t.rebours@gmail.com>
2020-10-04 21:39:37 +0200

    - Unified native I/O to hex strings ; - Added other encoding methods : 'legacy' which behaves like in v1.1.2, 'hex', 'base64' and 'buffer' - Update example with tests for a range of test vectors, removed Jest because it's unusable in this context (it's made for mocking native dependencies)

--- example/App.js
+++ example/App.js
@@ -7,18 +7,21 @@
  */
 
 import React, {Component} from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {ActivityIndicator, Button, StyleSheet, Text, View} from 'react-native'
 import scrypt from 'react-native-scrypt'
 import sjcl from './sjcl'
 import {randomBytes} from 'react-native-randombytes'
-
-const params = [16384, 8, 1]
-const length = 64
+import {assert} from 'chai'
+import {Buffer} from 'buffer'
+import testVectors from './test_vectors'
 
 function seeded() {
   return new Promise(resolve => {
-    if (sjcl.random.isReady()) resolve()
-    else sjcl.random.addEventListener('seeded', resolve)
+    if (sjcl.random.isReady()) {
+      resolve()
+    } else {
+      sjcl.random.addEventListener('seeded', resolve)
+    }
   })
 }
 
@@ -28,35 +31,156 @@ export default class App extends Component {
     this.state = {
       sjcl: '...',
       libscrypt: '...',
+      ready: false,
+      tests: 0,
+      passedTests: 0,
+    }
+  }
+
+  async test(name, callback) {
+    console.log(`Executing ${name}...`)
+    const start = Date.now()
+    try {
+      await callback()
+      console.log(`Test ${name} passed in ${Date.now() - start}ms`)
+      this.setState({tests: this.state.tests + 1, passedTests: this.state.passedTests + 1})
+    } catch (error) {
+      console.warn(`Test ${name} failed in ${Date.now() - start}ms with error`, error)
+      this.setState({tests: this.state.tests + 1})
     }
   }
 
   async componentDidMount() {
     await seeded()
-    const password = (await randomBytes(64)).toString('hex')
-    const salt = (await randomBytes(64)).toString('hex')
-    await this.libScrypt(password, salt)
-    await this.sjclScrypt(password, salt)
+    await this.generateRandomTestVector()
+    await this.runExample()
   }
 
-  async sjclScrypt(password, salt) {
-    const start = Date.now()
-    const sjclScrypt = sjcl.codec.hex.fromBits(
-      sjcl.misc.scrypt(
-        sjcl.codec.hex.toBits(password),
-        sjcl.codec.hex.toBits(salt),
-        ...params,
-        length * 8,
+  async generateRandomTestVector() {
+    // add a random test vector with non-ASCII characters
+    console.log('generating random test vector')
+    const testVector = {
+      password: await randomBytes(64),
+      salt: await randomBytes(64),
+      N: 16384,
+      r: 8,
+      p: 1,
+      dkLen: 64,
+      skipInLegacy: true,
+    }
+    testVector.expected = Buffer.from(
+      sjcl.codec.hex.fromBits(
+        sjcl.misc.scrypt(
+          sjcl.codec.hex.toBits(testVector.password.toString('hex')),
+          sjcl.codec.hex.toBits(testVector.salt.toString('hex')),
+          testVector.N,
+          testVector.r,
+          testVector.p,
+          testVector.dkLen * 8,
+        ),
       ),
+      'hex',
     )
-    this.setState({sjcl: `${sjclScrypt} ${Date.now() - start}ms`})
-    return sjclScrypt
+    testVectors.push(testVector)
+    console.log('random test vector generated')
   }
 
-  async libScrypt(password, salt) {
+  async runExample() {
+    console.log('running quick example', testVectors.length)
+    await this.sjclScrypt(testVectors[21])
+    console.log('control sjcl scrypt done')
+    await this.libScrypt(testVectors[21])
+    console.log('native scrypt done')
+    this.setState({ready: true})
+  }
+
+  async startTests() {
+    if (this.state.ready === false) {
+      return
+    }
+    this.setState({ready: false, tests: 0, passedTests: 0})
+    console.log('starting tests...')
+
+    for (let i = 0; i < testVectors.length; i++) {
+      const {password, salt, N, r, p, dkLen, expected, skipInLegacy = false} = testVectors[i]
+      if (!skipInLegacy) {
+        await this.test(`Test vector ${i + 1} with encoding legacy`, async () => {
+          const encodedPassword = password.toString('utf-8')
+          const encodedSalt = sjcl.codec.utf8String.toBits(salt.toString('utf-8'))
+          const actualResult = await scrypt(encodedPassword, encodedSalt, N, r, p, dkLen)
+          assert.strictEqual(actualResult, expected.toString('hex'))
+        })
+      }
+    }
+
+    for (let i = 0; i < testVectors.length; i++) {
+      const {password, salt, N, r, p, dkLen, expected} = testVectors[i]
+      await this.test(`Test vector ${i + 1} with encoding base64`, async () => {
+        const encodedPassword = password.toString('base64')
+        const encodedSalt = salt.toString('base64')
+        const actualResult = await scrypt(encodedPassword, encodedSalt, N, r, p, dkLen, 'base64')
+        assert.strictEqual(actualResult, expected.toString('base64'))
+      })
+    }
+
+    for (let i = 0; i < testVectors.length; i++) {
+      const {password, salt, N, r, p, dkLen, expected} = testVectors[i]
+      await this.test(`Test vector ${i + 1} with encoding hex`, async () => {
+        const encodedPassword = password.toString('hex')
+        const encodedSalt = salt.toString('hex')
+        const actualResult = await scrypt(encodedPassword, encodedSalt, N, r, p, dkLen, 'hex')
+        assert.strictEqual(actualResult, expected.toString('hex'))
+      })
+    }
+
+    for (let i = 0; i < testVectors.length; i++) {
+      const {password, salt, N, r, p, dkLen, expected} = testVectors[i]
+      await this.test(`Test vector ${i + 1} with encoding buffer`, async () => {
+        const encodedPassword = password
+        const encodedSalt = salt
+        const actualResult = await scrypt(encodedPassword, encodedSalt, N, r, p, dkLen, 'buffer')
+        assert.isTrue(actualResult.equals(expected))
+      })
+    }
+
+    if (this.state.tests === this.state.passedTests) {
+      console.log(`All ${this.state.tests} passed.`)
+    } else {
+      console.error(
+        `${this.state.passedTests} / ${this.state.tests} passed, ${this.state.tests - this.state.passedTests} failed.`,
+      )
+    }
+    this.setState({ready: true})
+  }
+
+  async sjclScrypt(testVector) {
+    const password = sjcl.codec.hex.toBits(testVector.password.toString('hex'))
+    const salt = sjcl.codec.hex.toBits(testVector.salt.toString('hex'))
+    const N = testVector.N
+    const r = testVector.r
+    const p = testVector.p
+    const dkLen = testVector.dkLen * 8
+
     const start = Date.now()
-    const libScrypt = await scrypt(password, salt, ...params, length)
-    this.setState({libscrypt: `${libScrypt} ${Date.now() - start}ms`})
+    const sjclScrypt = sjcl.misc.scrypt(password, salt, N, r, p, dkLen)
+    const elapsed = Date.now() - start
+
+    this.setState({sjcl: `${sjcl.codec.hex.fromBits(sjclScrypt)} ${elapsed}ms`})
+  }
+
+  async libScrypt(testVector) {
+    const password = testVector.password.toString('utf-8')
+    const salt = sjcl.codec.hex.toBits(testVector.salt.toString('hex'))
+    const N = testVector.N
+    const r = testVector.r
+    const p = testVector.p
+    const dkLen = testVector.dkLen
+
+    const start = Date.now()
+    const libScrypt = await scrypt(password, salt, N, r, p, dkLen)
+    const elapsed = Date.now() - start
+
+    this.setState({libscrypt: `${libScrypt} ${elapsed}ms`})
     return libScrypt
   }
 
@@ -64,9 +188,11 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>{`sjcl: ${this.state.sjcl}`}</Text>
-        <Text style={styles.text}>
-          {`react-native-scrypt: ${this.state.libscrypt}`}
-        </Text>
+        <Text style={styles.text}>{`react-native-scrypt: ${this.state.libscrypt}`}</Text>
+        <Button onPress={() => this.startTests()} title="Start tests" disabled={!this.state.ready} />
+        <ActivityIndicator animating={!this.state.ready} />
+        <Text style={styles.text}>{`${this.state.passedTests} / ${this.state.tests} passed`}</Text>
+        <Text style={styles.text}>{`${this.state.tests - this.state.passedTests} / ${this.state.tests} failed`}</Text>
       </View>
     )
   }
--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 import { NativeModules } from 'react-native'
+import { Buffer } from 'buffer'
 
 const { RNScrypt } = NativeModules
 
-export default async function scrypt (passwd, salt, N=16384, r=8, p=1, dkLen=64) {
-  return RNScrypt.scrypt(passwd, salt, N, r, p, dkLen)
+/**
+ * Copied from SJCL, bitArray.js#bitLength and bitArray.js#getPartial
+ * Find the length of an array of bits.
+ * @param {sjcl.bitArray} array The array.
+ * @return {Number} The length of a, in bits.
+ */
+const bitLength = array => {
+  const l = array.length
+  if (l === 0) return 0
+  const x = array[l - 1]
+  return (l - 1) * 32 + (Math.round(x / 0x10000000000) || 32)
+}
+/**
+ * Inspired from SJCL's codecBytes.js#frombits
+ * @param {sjcl.bitArray} array The array.
+ * @returns {Buffer}
+ */
+const bitArrayToBuffer = array => {
+  const bl = bitLength(array)
+  const out = Buffer.alloc(bl / 8)
+  let tmp
+  for (let i = 0; i < bl / 8; i++) {
+    if ((i & 3) === 0) tmp = array[i / 4]
+    out[i] = tmp >>> 24
+    tmp <<= 8
+  }
+  return out
+}
+
+export default async function scrypt (passwd, salt, N = 16384, r = 8, p = 1, dkLen = 64, encoding = 'legacy') {
+  let encodedInput = passwd
+  let encodedSalt = salt
+  if (encoding === 'legacy') {
+    encodedInput = Buffer.from(passwd, 'utf-8').toString('hex')
+    encodedSalt = bitArrayToBuffer(salt).toString('hex')
+  } else if (encoding === 'base64') {
+    encodedInput = Buffer.from(passwd, 'base64').toString('hex')
+    encodedSalt = Buffer.from(salt, 'base64').toString('hex')
+  } else if (encoding === 'buffer') {
+    encodedInput = passwd.toString('hex')
+    encodedSalt = salt.toString('hex')
+  } else if (encoding !== 'hex') throw new Error(`Encoding ${encoding} is invalid, it must be 'legacy', 'base64', 'hex' or 'buffer'`)
+
+  const result = await RNScrypt.scrypt(encodedInput, encodedSalt, N, r, p, dkLen)
+  if (encoding === 'base64') return Buffer.from(result, 'hex').toString('base64')
+  else if (encoding === 'buffer') return Buffer.from(result, 'hex')
+  else return result
 }
